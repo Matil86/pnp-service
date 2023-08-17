fd026bfa813d0835da4d86f2715425cead09664f
Martin Hipp <hipp@sipgate.de>
2023-08-17 10:09:18 +0200

    refactor rabbitmq host

--- rabbitmq-producer/src/main/java/de/hipp/pnp/rabbitmq/RabbitMQProducerConfiguration.java
+++ rabbitmq-producer/src/main/java/de/hipp/pnp/rabbitmq/RabbitMQProducerConfiguration.java
@@ -4,12 +4,19 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.AbstractConnectionFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:rabbitMQ.yml")
 public class RabbitMQProducerConfiguration {
 
+	@Value("${spring.rabbitmq.host}")
+	String host;
 	static final String TOPIC_EXCHANGE_CHARACTER_CREATION = "characterGeneration";
 	static final String QUEUE_GET_ALL_CHARACTERS = "GET_ALL_GENEFUNK";
 
@@ -38,5 +45,13 @@ public class RabbitMQProducerConfiguration {
 	@Bean
 	Binding createGetAllCharactersBindung(Queue getAllGeneFunkQueue, TopicExchange exchange) {
 		return BindingBuilder.bind(getAllGeneFunkQueue).to(exchange).with(exchange.getName());
+	}
+
+	@Bean
+	public CachingConnectionFactory ccf() {
+		CachingConnectionFactory ccf = new CachingConnectionFactory();
+		ccf.setAddresses(host + ":5672");
+		ccf.setAddressShuffleMode(AbstractConnectionFactory.AddressShuffleMode.INORDER);
+		return ccf;
 	}
 }

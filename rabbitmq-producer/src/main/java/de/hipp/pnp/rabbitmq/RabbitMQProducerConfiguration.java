d95561df44716d9482c48d947ec2b155b3189a1b
Martin Hipp <hipp@sipgate.de>
2023-08-17 14:21:32 +0200

    let's try the new rabbitmq cloud instance

--- rabbitmq-producer/src/main/java/de/hipp/pnp/rabbitmq/RabbitMQProducerConfiguration.java
+++ rabbitmq-producer/src/main/java/de/hipp/pnp/rabbitmq/RabbitMQProducerConfiguration.java
@@ -9,10 +9,8 @@ import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:rabbitMQ.yml")
 public class RabbitMQProducerConfiguration {
 
 	@Value("${spring.rabbitmq.host}")

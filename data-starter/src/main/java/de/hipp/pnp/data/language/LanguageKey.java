26c0f1269a4b66dfc6697bd9312cdb5cc9d505f3
Martin Hipp <hipp@sipgate.de>
2023-11-24 15:04:20 +0100

    loading languageKeys out of Yaml

--- data-starter/src/main/java/de/hipp/pnp/data/language/LanguageKey.java
+++ data-starter/src/main/java/de/hipp/pnp/data/language/LanguageKey.java
@@ -1,22 +1,14 @@
 package de.hipp.pnp.data.language;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class LanguageKey {
 	String label;
 	String description;
 
-	public String getLabel() {
-		return label;
-	}
-
-	public void setLabel(String label) {
-		this.label = label;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
 }
--- data-starter/src/main/java/de/hipp/pnp/data/language/LanguageKeyConfiguration.java
+++ data-starter/src/main/java/de/hipp/pnp/data/language/LanguageKeyConfiguration.java
@@ -1,29 +1,20 @@
 package de.hipp.pnp.data.language;
 
-import de.hipp.pnp.data.MultipleYamlPropertySourceFactory;
-import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Configuration
+@Setter
+@Getter
 @Slf4j
-@ConfigurationProperties
-@PropertySources({
-		@PropertySource(value = "classpath:genefunk/crb/languagekeys.yaml", factory = MultipleYamlPropertySourceFactory.class)
-})
+@Configuration
+@ConfigurationProperties(prefix = "locale")
 public class LanguageKeyConfiguration {
+	private Map<String, Map<String, LanguageKey>> enUS = new HashMap<>();
 
-	private Map<String, Map<String, Map<String, LanguageKey>>> locale = new HashMap<>();
-
-	@PostConstruct
-	public void init() {
-		log.info("LanguageKeyConfiguration initialized");
-		log.info("LanguageKeyConfiguration: " + locale);
-	}
 }

817257ad8529250c9243cda5370ae9e6a7e59451
Martin Hipp <hipp@sipgate.de>
2024-05-10 14:04:12 +0200

    🚀 first start of using oauth2 from google *yay*

--- security/src/main/java/de/hipp/pnp/security/SecurityConfiguration.java
+++ security/src/main/java/de/hipp/pnp/security/SecurityConfiguration.java
@@ -6,10 +6,19 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -20,11 +29,18 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         http
                 .cors(withDefaults())
+                .oauth2Login(o -> o
+                        .userInfoEndpoint(userInfoEndpointConfig ->
+                                userInfoEndpointConfig.userAuthoritiesMapper(this.userAuthoritiesMapper()))
+                        .failureHandler((request, response, exception) -> {
+                            request.getSession().setAttribute("error.message", exception.getMessage());
+                        }))
                 .authorizeHttpRequests((auth) ->
                         auth
-                                .requestMatchers("/", "/index.html").permitAll()
+                                .requestMatchers("/", "/index.html", "/login/**", "/oauth2/**", "/logout").permitAll()
                                 .requestMatchers("/resource/**")
                                 .hasAnyRole(Arrays.toString(Roles.values()))
                                 .requestMatchers("/admin/**")
@@ -39,9 +55,33 @@ public class SecurityConfiguration {
         return http.build();
     }
 
+    private GrantedAuthoritiesMapper userAuthoritiesMapper() {
+        return (authorities) -> {
+            Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
+
+            authorities.forEach(authority -> {
+                if (authority instanceof OidcUserAuthority oidcUserAuthority) {
+
+                    OidcIdToken idToken = oidcUserAuthority.getIdToken();
+                    OidcUserInfo userInfo = oidcUserAuthority.getUserInfo();
+                    Map<String, Object> attributes = oidcUserAuthority.getAttributes();
+
+                    mappedAuthorities.add(new SimpleGrantedAuthority("User"));
+
+                    // Map the claims found in idToken and/or userInfo
+                    // to one or more GrantedAuthority's and add it to mappedAuthorities
+
+                }
+            });
+
+            return mappedAuthorities;
+        };
+    }
+
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.debug(true);
+        return (web) -> web.debug(false);
     }
+
 
 }

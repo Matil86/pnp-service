3080a61fb4376a4cb467b878a7e030aca66b840a
Martin Hipp <hipp@sipgate.de>
2024-08-16 10:40:01 +0200

    switching data-starter to kotlin

---
+++ data-starter/src/main/kotlin/de/hipp/pnp/DataServiceApplication.kt
@@ -0,0 +1,12 @@
+package de.hipp.pnp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication(scanBasePackages = ["de.hipp.*"])
+open class DataServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<DataServiceApplication>(*args)
+}
+

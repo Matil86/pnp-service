de107dbe0f276b8804e6f82cf96c28dd678a0b1a
Martin Hipp <hipp@sipgate.de>
2025-06-20 12:06:58 +0200

    🐛 fixing startup bug for CharacterGeneratorApplication.kt

--- character-generator-starter/src/main/kotlin/de/hipp/pnp/CharacterGeneratorApplication.kt
+++ character-generator-starter/src/main/kotlin/de/hipp/pnp/CharacterGeneratorApplication.kt
@@ -4,8 +4,12 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication(scanBasePackages = ["de.hipp.*"])
-open class CharacterGeneratorApplication()
-
-fun main(args: Array<String>) {
-    SpringApplication.run(CharacterGeneratorApplication::class.java, *args)
+open class CharacterGeneratorApplication() {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            SpringApplication.run(CharacterGeneratorApplication::class.java, *args)
+        }
+    }
 }
+

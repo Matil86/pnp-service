b6d2b5ccf8c1b02c5ac1e2e5e78300f8c7818f60
Martin Hipp <hipp@sipgate.de>
2025-08-15 13:26:41 +0200

    let's build a docker image as monolith

--- base/src/main/java/de/hipp/pnp/base/entity/CharacterSpeciesEntity.kt
+++ base/src/main/java/de/hipp/pnp/base/entity/CharacterSpeciesEntity.kt
@@ -1,9 +1,12 @@
 package de.hipp.pnp.base.entity
 
 import de.hipp.pnp.base.fivee.Feature5e
+import jakarta.persistence.CascadeType
 import jakarta.persistence.ElementCollection
 import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
 import jakarta.persistence.Id
+import jakarta.persistence.OneToMany
 import org.springframework.boot.context.properties.bind.ConstructorBinding
 import org.springframework.stereotype.Component
 import java.io.Serializable
@@ -16,13 +19,13 @@ data class CharacterSpeciesEntity @ConstructorBinding constructor(
     var description: String = "",
     @ElementCollection
     var attributes: Map<String, String> = emptyMap(),
-    @ElementCollection(targetClass = Feature5e::class)
-    var features: List<Feature5e> = emptyList()
+    @OneToMany(targetEntity = Feature5e::class, fetch = FetchType.EAGER, cascade = [CascadeType.ALL])
+    var features: MutableList<Feature5e> = mutableListOf<Feature5e>()
 ) : Serializable {
     constructor() : this(
         name = "",
         description = "",
         attributes = emptyMap(),
-        features = emptyList()
+        features = mutableListOf<Feature5e>()
     )
 }

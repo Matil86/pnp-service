1265c611e31135b0725d4beb6d1a6393a7f11630
Martin Hipp <hipp@sipgate.de>
2025-08-15 16:05:41 +0200

    lets see those beans

--- monolith/src/main/kotlin/de/hipp/pnp/CharacterGeneratorApplication.kt
+++ monolith/src/main/kotlin/de/hipp/pnp/CharacterGeneratorApplication.kt
@@ -1,7 +1,12 @@
 package de.hipp.pnp
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.Bean
+import java.util.Arrays
+
 
 @SpringBootApplication
 class CharacterGeneratorApplication {
@@ -10,6 +15,17 @@ class CharacterGeneratorApplication {
         @JvmStatic
         fun main(args: Array<String>) {
             SpringApplication.run(CharacterGeneratorApplication::class.java, *args)
+        }
+
+        @Bean
+        fun commandLineRunner(ctx: ApplicationContext): CommandLineRunner {
+            return CommandLineRunner { args: Array<String?>? ->
+                println("Let's inspect the beans provided by Spring Boot:")
+                val beanNames: List<String?> = ctx.getBeanDefinitionNames().filter { bean -> !bean.toString().contains("spring") }
+                for (beanName in beanNames) {
+                    println(beanName)
+                }
+            }
         }
     }
 }

d434276ba26ae737eaf042c8e34c681ac5038a12
Martin Hipp <hipp@sipgate.de>
2025-08-19 16:48:36 +0200

    let's move the main class

--- character-generator-starter/src/main/kotlin/de/hipp/pnp/CharacterGeneratorApplication.kt
+++ character-generator-starter/src/main/kotlin/de/hipp/pnp/CharacterGeneratorApplication.kt
@@ -5,8 +5,6 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Bean
-import java.util.Arrays
-
 
 @SpringBootApplication
 class CharacterGeneratorApplication {
@@ -21,8 +19,8 @@ class CharacterGeneratorApplication {
         fun commandLineRunner(ctx: ApplicationContext): CommandLineRunner {
             return CommandLineRunner { args: Array<String?>? ->
                 println("Let's inspect the beans provided by Spring Boot:")
-                val beanNames: List<String?> = ctx.getBeanDefinitionNames().filter { bean -> !bean.toString().contains("spring") }
-                Arrays.sort(beanNames.toTypedArray())
+                val beanNames: List<String?> =
+                    ctx.getBeanDefinitionNames().filter { bean -> !bean.toString().contains("spring") }
                 for (beanName in beanNames) {
                     println(beanName)
                 }
